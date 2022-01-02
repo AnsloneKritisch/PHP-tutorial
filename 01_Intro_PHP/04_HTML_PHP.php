ca75368bb857e5b928eee39d2b932f078e82f2f0
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-02 21:51:51 +0530

    New

--- 01_Intro_PHP/04_HTML_PHP.php
+++ 01_Intro_PHP/04_HTML_PHP.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php echo "Hello World" ; echo "Titile";?>   </title>
+    <title> HTML and PHP  </title>
 </head>
 <body>
     <?php echo "Hello World" ; ?>
