b68e59b2bd5a79e313a9f0a5e0e3a18fd79c2895
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-04 13:43:19 +0530

    variable

--- 01_Intro_PHP/01_Intro.php
+++ 01_Intro_PHP/01_Intro.php
@@ -1,6 +1,7 @@
 <?php
 
 echo "Hello World" ;
+echo " PHP is a case sensetive language " ;
 echo 58+9 ;
 
 ?>
--- 01_Intro_PHP/06_Variable.php
+++ 01_Intro_PHP/06_Variable.php
@@ -1,10 +1,18 @@
-//The best way to add variable
 <?php
+
+// This is best way to add variable.
+/*
+Rules for using Variable are :
+    1. Variable name should start with $
+    2. Keywords not allowed
+    3. Space not allowed
+    4. Special character not allowed except underscore.
+*/
 
 $a = 10 ;
 $b = 20 ;
 
-echo ("The Number in Varible a is = ", $a) ;
+echo "The Number in Varible a is = ", $a ;
 
 
 
