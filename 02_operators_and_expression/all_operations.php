5b27b04df0784133f6cb29f78c08d5259e0e4277
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-05 17:21:38 +0530

    operator

--- 02_operators_and_expression/all_operations.php
+++ 02_operators_and_expression/all_operations.php
@@ -7,7 +7,11 @@ $e = $b / $a ;
 $f = $a * $b ;
 $g = $b % $a ;
 
-echo "The sum of " , $a , " and " , $a , " is = " , $c ;
+echo "The sum of " , $a , " and " , $b , " is = " , $c ;
+echo "The difference of " , $b , " and " , $a , " is = " , $d ;
+echo "The product of " , $a , " and " , $b , " is = " , $f ;
+echo "The division of " , $b , " and " , $a , " is = " , $e ;
+echo "The remainder of " , $b , " and " , $a , " after division is = " , $e ;
 
 
 ?>
