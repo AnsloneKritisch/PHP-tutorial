067d1c43eeb99b9db1de345209eb38ed8722b2ad
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-12 13:54:54 +0530

    New

--- 03_Flow_of_Control/02_for_loop/06_prime_no_range.php
+++ 03_Flow_of_Control/02_for_loop/06_prime_no_range.php
@@ -4,23 +4,23 @@ $starting = 1 ;
 $ending = 20 ;
 $c = 0 ;
 
-echo " All the Prime numbers between the range " , $starting , " and " , $ending , " are " ;
+echo " All the Prime numbers between the range " , $starting , " and " , $ending , " are >>> " ;
 
 for ($i=$starting; $i <= $ending ; $i++)
 { 
     $c = 0 ;
+    
     for ($j = 1; $j <= $i ; $j++)
     { 
         if($i % $j == 0)
         {
             $c = $c + 1 ;
         }
-
     }
+
     if ($c == 2)
     {
-        echo $i ;
-    }
-    
+        echo $i , " ; " ;
+    }  
 }
 ?>
