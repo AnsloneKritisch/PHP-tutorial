bf46b82d6b67c39538fa1c83b317803bac319560
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-22 15:32:04 +0530

    new

--- 04_Array_and_Subscripted_variables/1d_Array/01_basic.php
+++ 04_Array_and_Subscripted_variables/1d_Array/01_basic.php
@@ -3,9 +3,10 @@
 $a = array(10,20,30,40,50) ;
 $b = count($a) ;
 echo "Number Of array ", $b ;
+echo "<br> All the numbers in the array are >>> " ;
 for ($i=0; $i < $b ; $i++)
 { 
-    echo $a[$i] ;
+    echo $a[$i] , ";" ;
 }
 
 ?>
