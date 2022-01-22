082701e38a5f93dd893f22ce5633b66ac0c34b67
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-22 10:05:19 +0530

    New

--- 03_Flow_of_Control/02_for_loop/11_perfect_no.php
+++ 03_Flow_of_Control/02_for_loop/11_perfect_no.php
@@ -1,11 +1,26 @@
 <?php
 
-$a = 123 ;
-for($i=1;i<=$a;$i++)
+$a = 6  ;
+$j = 0;
+
+for($i=1;$i<=$a;$i++)
 {
     if($a%$i==0)
     {
         $j = $j + 1 ;
     }
 }
+
+$b = $a * 2 ;
+
+if ($j == $b )
+{
+    echo $a , " is a Perfect Number " ;
+}
+
+else
+{
+    echo $a , " is not a Perfect Number " ;
+}
+
 ?>
