8407ad04d78818d671f079766e6c94f95a8ef7f4
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-02-18 22:45:56 +0530

    new

---
+++ 07_Super_Global_Variable/input_get.php
@@ -0,0 +1,30 @@
+<?php
+
+if (isset($_GET["rsub"]))
+{
+  $name = $_GET['rname'] ;
+  echo "Your name is ", $name ;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+  <style>
+    body{
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <form action="" method="GET">
+    Name : <input type="text" name="rname">
+    <br>
+    <br>
+    <input type="submit" value="Submit Now" name="rsub">
+  </form>
+</body>
+</html>
