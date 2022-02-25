e4f8d986d50f0526b2cc51a4c79e73164fb3f557
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-02-25 10:31:31 +0530

    new

---
+++ 08_SQL_Connection/05_Create_Input_Show_Table.php
@@ -0,0 +1,22 @@
+<!--
+    Objectives
+    ____________
+
+    1. We Will Create a Database
+    2. Create a Table
+    3. Take input from the form
+    4. Print all your data 
+
+ -->
+
+ 
+ 
+ <!-- Starting PHP codes for Creating connections with SQL  -->
+
+ <?php
+ 
+ 
+ 
+ ?>
+
+ <!-- Ending PHP codes for Creating connections with SQL  -->
